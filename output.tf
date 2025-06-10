d3aeb2ef1855682cd6b213f966585c181e2d28c1
Harish6498-git <harish.m6498@gmail.com>
2025-06-10 14:07:40 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -18,3 +18,18 @@ output "aws_internet_gateway_id" {
     value = aws_internet_gateway.igw.id
 }
 
+output "rds_endpoint" {
+  description = "RDS endpoint"
+  value       = aws_db_instance.rds.endpoint
+}
+
+output "rds_username" {
+  value = var.db_username
+}
+
+output "rds_password" {
+  value     = random_password.db_pass.result
+  sensitive = true
+}
+
+
